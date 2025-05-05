fc839049d97972163229dced8cac3a0755fccf52
Bremojy <brianmukuthu3@gmail.com>
2025-05-06 00:23:47 +0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,16 +11,26 @@ const Navbar = () => {
                  <div className="collapse navbar-collapse" id="ivy">
                     <div className="navbar-nav">
                         <Link to="/" className="nav-link">Home</Link>
-                        <Link to="/chatbot" className="nav-link"><b>Chatbot</b></Link>
-                        <Link to="/cart" className="nav-link">CART</Link>
+                        <Link to="/chatbot" className="nav-link fw-bold">Chatbot</Link>
+
+                        
                        
                         <Link to="/addproducts" className="nav-link">Add Products</Link>
                     </div>
-                    <div className="navbar-nav ms-auto">
-                        
+                    <div className="navbar-nav ms-auto d-flex align-items-center">
+                        <Link to="/cart" className="nav-link me-3">
+                            <img src="images/shopping-cart.png" style={{ height: "40px" }} alt="Cart" />
+                            <span
+                              className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger"
+                              style={{ fontSize: "0.75rem" }}>
+    
+                                {cartItemCount}
+                         </span>
+                        </Link>
                         <Link to="/signin" className="nav-link">Sign In</Link>
                         <Link to="/signup" className="nav-link">Sign Up</Link>
                     </div>
+
                     
                  </div>
             </div>
