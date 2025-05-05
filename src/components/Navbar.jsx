168ecb8ec5dbc12a4bc962d2b8a1171a49643809
Bremojy <brianmukuthu3@gmail.com>
2025-05-06 00:45:31 +0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,12 +20,7 @@ const Navbar = () => {
                     <div className="navbar-nav ms-auto d-flex align-items-center">
                         <Link to="/cart" className="nav-link me-3">
                             <img src="images/shopping-cart.png" style={{ height: "40px" }} alt="Cart" />
-                            <span
-                              className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger"
-                              style={{ fontSize: "0.75rem" }}>
-    
-                                {cartItemCount}
-                         </span>
+                            
                         </Link>
                         <Link to="/signin" className="nav-link">Sign In</Link>
                         <Link to="/signup" className="nav-link">Sign Up</Link>
